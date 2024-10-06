5c6dd696b25c582c97173a3f574b04a7a68f8bb9
3222h <394954a+++@gmail.com>
2024-10-06 21:27:54 +0530

    Update popup.js

--- IP-Info/popup.js
+++ IP-Info/popup.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
   const ipField = document.getElementById('ip');
   const countryField = document.getElementById('country');
   const copyButton = document.getElementById('copyButton');
+  const dRepoButton = document.getElementById('dRepoButton');
+  const rRepoButton = document.getElementById('rRepoButton');
 
   // Mapping of country codes to full country names
   const countryNames = {
@@ -30,9 +32,25 @@ document.addEventListener('DOMContentLoaded', function() {
       console.error(error);
     });
 
-  // Copy button functionality without confirmation
+  // Copy button functionality for Nomashine
   copyButton.addEventListener('click', function() {
-    const contentToCopy = "curl -sLkO https://is.gd/nomashine ; bash nomashine";
+    const contentToCopy = "curl -sLkO https://is.gd/nomashine ; bash nomashine\n";
+    navigator.clipboard.writeText(contentToCopy).catch(err => {
+      console.error('Could not copy text: ', err);
+    });
+  });
+
+  // Copy button functionality for D-REPO
+  dRepoButton.addEventListener('click', function() {
+    const contentToCopy = "git clone https://github.com/3222h/youtube.git\n";
+    navigator.clipboard.writeText(contentToCopy).catch(err => {
+      console.error('Could not copy text: ', err);
+    });
+  });
+
+  // Copy button functionality for R-Repo
+  rRepoButton.addEventListener('click', function() {
+    const contentToCopy = "git clone https://github.com/3222h/r-repo.git\n"; // Update with the actual URL for R-Repo
     navigator.clipboard.writeText(contentToCopy).catch(err => {
       console.error('Could not copy text: ', err);
     });
